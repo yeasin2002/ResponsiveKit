e942df8064664a02a7a0648f54487d3462173c30
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-12-04 15:43:24 +0600

    refactor: moved all project file in src and using app dir  instead of entrypointsDir

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react", "@wxt-dev/unocss"],
+  entrypointsDir: "src/app",
+
 });
