82867cd31ba684074d537e96c05799b56fd03cd2
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-12-04 16:30:03 +0600

    refactor: moved app dir to root

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react", "@wxt-dev/unocss"],
-  entrypointsDir: "src/app",
-
+  entrypointsDir: "app",
 });
