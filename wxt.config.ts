5a291012f6990ba3bd7954a422f9c2a97a8344d2
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-12-08 16:23:35 +0600

    chore: updated Manifest  icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ["@wxt-dev/module-react", "@wxt-dev/unocss", "@wxt-dev/auto-icons"],
-  entrypointsDir: "app",
-  manifest: {action: {}, },
+	modules: ["@wxt-dev/module-react", "@wxt-dev/unocss", "@wxt-dev/auto-icons"],
+	entrypointsDir: "app",
+	manifest: {
+		action: {}, // Empty action with no popup - enables browser.action.onClicked
+		name: "ResponsiveKit",
+		description: "Real-time responsive breakpoint indicator for developers",
+		icon: "ResponsiveKit.svg", // Auto-icons will generate all sizes from this
+	},
 });
